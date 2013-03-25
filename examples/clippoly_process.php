9591110f73ffdd6978ec9d05aeb8c866e52baf97
Alec Bennett <alec@alecbennett.com>
2013-03-25 11:17:58 -0800

    Add id to json file, remove shapefile conversion

--- examples/clippoly_process.php
+++ examples/clippoly_process.php
@@ -1,13 +1,15 @@
 <?php
 	$max = count($_POST['Shape']);
 	$mainString = '{ "type": "FeatureCollection", "features": [ { "type": "Feature",';
+	$mainString .= '"properties": { "prop0": "value0" },';
+	$mainString .= '"id": 0,';
 	$mainString .= ' "geometry": { ';
 	$mainString .= '"type": "Polygon", "coordinates": [ [';
 	$mainString .= ' [ '.$_POST['Shape'][0]['Point'].' ]';
 	for ($i = 1; $i < $max; $i++){
 		$mainString .= ', [ '.$_POST['Shape'][$i]['Point'].' ]';
 	}
-	$mainString .= ' ] ] }, "properties": { "prop0": "value0" } } ] }';
+	$mainString .= ' ] ] } } ] }';
 	$fileName = md5($SERVER['REMOTE_ADDR'].time());
 	file_put_contents ( "output/".$fileName.".json" , $mainString );
 	$sysout = "";
--- examples/clipwarp.sh
+++ examples/clipwarp.sh
@@ -5,6 +5,5 @@ OUTDIR=output
 PREFIX="output/$1"
 
 rm ${PREFIX}.tif
-ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:3338 ${PREFIX}.shp ${PREFIX}.json
-gdalwarp -dstnodata 255 -co COMPRESS=DEFLATE -co TILED=YES -of GTiff -r lanczos -crop_to_cutline -cutline ${PREFIX}.shp NALCMS.tif ${PREFIX}.tif
-#rm ${PREFIX}.json ${PREFIX}.shp ${PREFIX}.shx  ${PREFIX}.dbf  ${PREFIX}.prj
+gdalwarp -dstnodata 255 -co COMPRESS=DEFLATE -co TILED=YES -of GTiff -r lanczos -crop_to_cutline -cutline ${PREFIX}.json NALCMS.tif ${PREFIX}.tif
+#rm ${PREFIX}.json 
