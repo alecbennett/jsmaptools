1d65207ef78abf701a76045139e0926d3cb9572f
Alec Bennett <alec@alecbennett.com>
2013-04-11 10:23:57 -0800

    Rename of js lib, moving examples

--- examples/clippoly.php
+++ examples/clippoly.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" href="../css/style.css">
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"> </script>
 		<script  src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
-		<script type="text/javascript" src="../js/maps.js"></script>
+		<script type="text/javascript" src="../js/jsMapTools.js"></script>
 
 		<script type="text/javascript">
 
